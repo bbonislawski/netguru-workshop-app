a195fd8185b519ce510312ce79ef4a453e1f0a55
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2016-07-26 22:10:33 +0200

    Add routing for teachers/subjects

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :students do
     get :subjects
   end
-  resources :teachers
+  resources :teachers do
+    get :subjects
+  end
 
   resources :visitors, only: [:index]
 
