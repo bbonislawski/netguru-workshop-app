982db009147ae15d7d2e1161db88245de3fc00af
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2016-07-26 20:23:56 +0200

    Implement avg_notes in StudentDecorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,8 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = subject_item.subject_item_notes
+    return '0.00' if notes.length == 0
+    '%.02f' % (notes.inject(0) { |acc, n| acc + n.value } / notes.length.to_f)
   end
 end
